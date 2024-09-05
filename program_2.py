dcaaf476e083d4ed0565dcadecc0eb6d1fca6c42
trblair <trblair@students.unwsp.edu>
2024-09-05 16:24:27 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,12 +1,12 @@
 #This program was written on 8/29/2024 by Tristan Blair
 #Proverbs Display
-
+import 
 def proverbs_22_29():
     # Displays the bible verse Proverbs 22:29
     print('“Do you see a man skillful in his work?')
     print('He will stand before kings;')
     print('he will not stand before obscure men.”')
-    print('        Proverbs 22:29')
+    print("\tProverbs 22:29")
     
 
 proverbs_22_29()
