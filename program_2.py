a90683db2756f8b852a166b984b48632f60238b3
trblair <trblair@students.unwsp.edu>
2024-09-05 16:20:09 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -6,7 +6,7 @@ def proverbs_22_29():
     print('“Do you see a man skillful in his work?')
     print('He will stand before kings;')
     print('he will not stand before obscure men.”')
-    print('        (Proverbs 22:29)')
+    print('        Proverbs 22:29')
     
 
 proverbs_22_29()
